1c9559367592ab521c48d9f5ad5d8099937856ce
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-01 20:25:11 -0700

    feat: implement module __dir__()

--- tests/test_module_import.py
+++ tests/test_module_import.py
@@ -15,3 +15,10 @@ def test_attribute_error() -> None:
 
     assert "project_paths" in str(exc_info.value)
     assert "this_attribute_does_not_exist" in str(exc_info.value)
+
+
+def test_dir() -> None:
+    directory = dir(project_paths)
+
+    assert "paths" in directory
+    assert directory == sorted(directory), "dir(mod) must be sorted"
