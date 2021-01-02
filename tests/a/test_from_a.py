3c7d3c0d24d29a00d2a9fcf08cfb70a0ac6288ed
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-01 21:32:41 -0700

    docs: better docs for a/b tests

--- tests/a/test_from_a.py
+++ tests/a/test_from_a.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""
-Tests that this module gets its configuration from its pyproject.toml file.
-"""
-
 from pathlib import Path
+
+from project_paths import paths
 
 
 def test_from_a():
-    from project_paths import paths
+    """
+    Tests that this module gets its configuration from its pyproject.toml file.
+    See also test_from_b() which is the exact same test, but loads b instead of a.
+    This exercises the following features:
+     - infer correct pyproject.toml from calling module
+     - base relative paths relative to pyproject.toml
+    """
 
     assert paths.filename.resolve().samefile(Path(__file__))
--- tests/b/test_from_b.py
+++ tests/b/test_from_b.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""
-Same test as test_from_a, but making sure that a DIFFERENT pyproject.toml gets loaded
-"""
-
 from pathlib import Path
 
+from project_paths import paths
 
-def test_from_a():
-    from project_paths import paths
+
+def test_from_b():
+    """
+    Same test as test_from_a, but making sure that a DIFFERENT pyproject.toml gets loaded
+    """
 
     assert paths.filename.resolve().samefile(Path(__file__))
