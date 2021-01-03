1b453bd039288cb46631cb1e2ab00bfe6317a4fa
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-02 20:33:27 -0700

    test: _ProxyPaths __repr__

--- tests/test_repr.py
+++ tests/test_repr.py
@@ -14,3 +14,13 @@ def test_concrete_repr():
 
     assert type(paths).__qualname__ in repr(paths)
     assert str(toml_path) in repr(paths)
+
+
+def test_proxy_repr():
+    """"""
+    toml_path = project_paths.find_caller_relative_path_to_pyproject()
+    concrete_paths = project_paths._ConcretePaths(toml_path)
+    paths_proxy = project_paths.paths
+
+    assert type(paths_proxy).__qualname__ in repr(paths_proxy)
+    assert repr(concrete_paths) in repr(paths_proxy)
